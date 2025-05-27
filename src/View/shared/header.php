e3853a261aaf8d6309854489eb725cec4eb0f704
David Santos <david.tecds@gmail.com>
2025-05-27 16:40:58 -0300

    Bug not solved
    O bug não foi resolvido, mas logo logo eu consigo

--- src/View/shared/header.php
+++ src/View/shared/header.php
@@ -5,8 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
 
-    <link rel="stylesheet" href="Lib/css/styleall.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+    <style> 
+
+    .container {
+    display: flex;
+    width: 100%; 
+    height: 120px;
+    background-color: whitesmoke;
+    }
+
+    .boxleft{
+
+        flex: 1;
+        align-items: center;
+        display: flex;
+        
+    }
+
+    .boxright{
+
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        margin: 20px;
+
+    }
+
+
+
+    </style>
 
 </head>
 <body>
@@ -22,23 +51,16 @@
         <i class="fa-brands fa-instagram"></i>
 
          </div>
-    <div class="boxright"></div>
+    <div class="boxright">
 
-    <ul>
         
-        <a href=""> <li class="inner">
-            Nome
-        </li> </a>
+        <ul>
+    <li class="inner"><a href="">Nome</a></li>
+    <li class="inner"><a href="">Nome</a></li>
+    <li class="inner"><a href="">Nome</a></li>
+</ul>
 
-        <a href=""> <li class="inner">
-            Nome
-        </li> </a>
-            
-        <a href=""> <li class="inner">
-            Nome
-        </li> </a>
         
-    </ul>
 
 
         </div>
