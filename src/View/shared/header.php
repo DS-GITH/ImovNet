4c357ecf305becef725a476fe98d5b8d00c41b26
David Santos <david.tecds@gmail.com>
2025-05-27 14:44:14 -0300

    Criando header
    Alguns bugs para o css precisam ser resolvidos

---
+++ src/View/shared/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <link rel="stylesheet" href="Lib/css/styleall.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+
+</head>
+<body>
+
+ <header>
+    <div class="container">
+        <div class="boxleft">
+
+        <h1> <span> ImovNet </span> </h1>
+
+        <i class="fa-brands fa-facebook"></i>
+        <i class="fa-brands fa-whatsapp"></i>
+        <i class="fa-brands fa-instagram"></i>
+
+         </div>
+    <div class="boxright"></div>
+
+    <ul>
+        
+        <a href=""> <li class="inner">
+            Nome
+        </li> </a>
+
+        <a href=""> <li class="inner">
+            Nome
+        </li> </a>
+            
+        <a href=""> <li class="inner">
+            Nome
+        </li> </a>
+        
+    </ul>
+
+
+        </div>
+ </header>
+    
+</body>
+</html>
